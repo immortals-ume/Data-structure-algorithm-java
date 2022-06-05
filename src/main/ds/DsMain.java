1b6dbaa03900b310fabe581f9f14a7a7d77ff68b
immortals-ume <kaish127799@outlook.com>
2022-06-05 19:45:17 +0530

    ✨ Added implementation of Tree Data Structure

--- src/main/ds/DsMain.java
+++ src/main/ds/DsMain.java
@@ -1,6 +1,7 @@
 package main.ds;
 
 import main.ds.stack.Stack;
+import main.ds.tree.Tree;
 
 import static main.utils.RandomUtils.randomInt;
 
@@ -26,6 +27,12 @@ public class DsMain{
         System.out.println( stack.pop( ) );
 
         System.out.println( "TREE DATA STRUCTURE" );
+        Tree tree=new Tree( );
+        tree.BinarySearchTree( 5 );
+        tree.BinarySearchTree( 6 );
+        tree.BinarySearchTree( 4 );
+        tree.BinarySearchTree( 52 );
+        tree.BinarySearchTree( 54 );
 
     }
 }
--- src/main/ds/tree/Tree.java
+++ src/main/ds/tree/Tree.java
@@ -3,11 +3,55 @@ package main.ds.tree;
 public class Tree{
 
 
-    private static void createTree( TreeNode treeNode,int data ){
+    private TreeNode root;
 
+    private TreeNode createTree( TreeNode current,int data ){
+
+        if ( current==null ) {
+            return new TreeNode( data );
+        }
+
+        if ( data<=current.data ) {
+            current.left=createTree( current.left,data );
+        } else
+            if ( data>=current.data ) {
+                current.right=createTree( current.right,data );
+            } else {
+                return current;
+            }
+
+        return current;
     }
 
-    public static void BinarySearchTree(){
+    public void BinarySearchTree( int data ){
+        root=createTree( root,data );
+    }
 
+    public void traverseInOrder( TreeNode node ){
+        if ( node!=null ) {
+            traverseInOrder( node.left );
+            visit( node );
+            traverseInOrder( node.right );
+        }
+    }
+
+    private void visit( TreeNode node ){
+        System.out.print( " "+node.data );
+    }
+
+    public void traversePreOrder( TreeNode node ){
+        if ( node!=null ) {
+            visit( node );
+            traverseInOrder( node.left );
+            traverseInOrder( node.right );
+        }
+    }
+
+    public void traversePostOrder( TreeNode node ){
+        if ( node!=null ) {
+            traverseInOrder( node.left );
+            traverseInOrder( node.right );
+            visit( node );
+        }
     }
 }
